39470fbd4fda1f7bc515ee9c16ea79692b084309
marychris20 <marychrisminted@gmail.com>
2024-06-13 13:49:49 +0800

    Add files via upload

---
+++ login.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
+    <title>LOG IN</title>
+    <link rel="stylesheet" href="login.css">
+
+<body>
+    <div class="login-form">
+        <form action=".php" method="post"></form>
+
+        <h1>Log In</h1>
+
+        <form>
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username">
+            </div>
+
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password">
+            </div>
+
+            <div class="remember-forgot">
+                <label><input type="checkbox">Remember Me</label><br>
+                <a href="a">Forgot Password</a>
+            </div>
+
+            <div class="form-group">
+                <button type="submit">Login</button>
+                <link rel="stylesheet" href="create.php">
+
+
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
